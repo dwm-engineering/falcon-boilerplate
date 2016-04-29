162e107e0c68c8c232d821d47e62816cf7881784
JA Arce <ja@directworksmedia.com>
2016-04-30 07:40:08 +0800

    Added Startapp

--- src/server.py
+++ src/server.py
@@ -1,8 +1,26 @@
 from wsgiref import simple_server
-
 from app import app
+import subprocess
+import sys
+
+
+def runserver():
+    httpd = simple_server.make_server('127.0.0.1', 8002, app)
+    httpd.serve_forever()
+
+
+def startapp(app_name):
+    create_command = 'mkdir resources/%s' % app_name
+    subprocess.call(create_command.split(' '))
+    open('resources/%s/controller.py' % app_name, 'a')
+    open('resources/%s/__init__.py' % app_name, 'a')
+    open('resources/%s/models.py' % app_name, 'a')
+    open('resources/%s/serializers.py' % app_name, 'a')
 
 
 if __name__ == '__main__':
-    httpd = simple_server.make_server('127.0.0.1', 8002, app)
-    httpd.serve_forever()
+    if len(sys.argv) > 2:
+        if sys.argv[1] == 'startapp':
+            startapp(sys.argv[2])
+    else:
+        runserver()
